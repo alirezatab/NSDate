7630636f5fd22bd3b904a2ff7446846c554056e9
alirezatab <alirezatabrizi10@gmail.com>
2017-01-05 11:01:13 -0800

    timeZone converstion done, using specifiers and changing date to string adn vice-versa

--- NSDate.playground/Contents.swift
+++ NSDate.playground/Contents.swift
@@ -14,11 +14,12 @@ let dateFormatter = DateFormatter()
 //ateFormatter uses locale settings of the device by default, unless another locale explicitly set
 dateFormatter.locale = NSLocale.current
 
-// setting locale - Greek and French
-///dateFormatter.locale = NSLocale(localeIdentifier: "el_GR") as Locale!
-///dateFormatter.locale = NSLocale(localeIdentifier: "fr_FR") as Locale!
 
-//  NSDateFormatterStyle enum. Each single value of it represents a different type of formatting
+
+/////////////////////////////////////////////////////////////////////
+///  NSDateFormatterStyle enum. Each single value of it represents a different type of formatting
+///////////////////////////////////////////////////////////////////////
+
 //fullstyle
 dateFormatter.dateStyle = DateFormatter.Style.full
 var convertedDate = dateFormatter.string(from: currentDate as Date)
@@ -30,5 +31,45 @@ convertedDate = dateFormatter.string(from: currentDate as Date)
 //Medium style
 dateFormatter.dateStyle = DateFormatter.Style.medium
 convertedDate = dateFormatter.string(from: currentDate as Date)
+
+///usin different locales
+dateFormatter.dateStyle = DateFormatter.Style.full
+
+// current locale
+dateFormatter.locale = NSLocale.current
+convertedDate = dateFormatter.string(from: currentDate as Date)
+
+// Greek Locale
+dateFormatter.locale = NSLocale(localeIdentifier: "el_GR") as Locale!
+convertedDate = dateFormatter.string(from: currentDate as Date)
+
+// French Locale
+dateFormatter.locale = NSLocale(localeIdentifier: "fr_FR") as Locale!
+convertedDate = dateFormatter.string(from: currentDate as Date)
+
+
+
+///////////////////////////////////////
+///using specifiers to formate the date
+////////////////////////////////////////
+dateFormatter.locale = NSLocale.current
+dateFormatter.dateFormat = "EEEE, MMMM dd, yyyy"
+convertedDate = dateFormatter.string(from: currentDate as Date)
+
+dateFormatter.dateFormat = "HH:mm:ss"
+convertedDate = dateFormatter.string(from: currentDate as Date)
+
+// converting from String to NSDate
+var dateAsString = "24-12-2015 23:59"
+dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+var newDate = dateFormatter.date(from: dateAsString)
+
+// more complex with timezone included
+//change between 9:22 to 12:22 is cause of specfied GMT Time zone
+dateAsString = "Thu, 08 Oct 2015 09:22:33 GMT"
+dateFormatter.dateFormat = "EEEE, dd MM yyyy HH:mm:ss zzz"
+newDate = dateFormatter.date(from: dateAsString)
+
+
 
 
